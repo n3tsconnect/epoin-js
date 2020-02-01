e8a8e7f7be57608318005ea2e5e9abfb575e1632
adrianardizza <adrian.ardizza@gmail.com>
2020-02-02 03:28:16 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 module.exports = (passport) => {
-    var allowed = [
-        "112778240035515513676",
-    ]
+    var allowed = []
 
     passport.serializeUser((user, done) => {
         done(null, user);
@@ -11,9 +9,9 @@ module.exports = (passport) => {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
-            clientID: "913025732460-12klt42ibnronhehhcutju930be1ccrn.apps.googleusercontent.com",
-            clientSecret: "s67Rf6cVoa0lXicwjVqFJ4YZ",
-            callbackURL: "http://localhost:3000/auth/google/callback"
+            clientID: "CLIENT ID HERE",
+            clientSecret: "CLIENT SECRET HERE",
+            callbackURL: "http://*SERVER IP ADDRESS HERE*/auth/google/callback"
         },
         (token, refreshToken, profile, done) => {
             console.log(profile);
