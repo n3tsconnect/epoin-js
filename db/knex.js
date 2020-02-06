3fd227f55bc521b35ab79a0999a8eb6e380181ef
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-02-06 16:55:18 +0700

    Added datestring value to knex conf

--- db/knex.js
+++ db/knex.js
@@ -4,7 +4,8 @@ const knex = require('knex')({
         host     : process.env.DB_HOST,
         user     : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
-        database : process.env.DB_DBNAME
+        database : process.env.DB_DBNAME,
+        dateStrings:true
     }
   });
 
